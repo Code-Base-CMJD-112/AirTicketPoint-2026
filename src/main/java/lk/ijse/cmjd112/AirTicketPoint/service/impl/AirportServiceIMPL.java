2874c6eb0355fe9f10b368694e40971afde07e6a
ThanuraSilva <thanurasilva1989@gmail.com>
2026-01-17 13:55:57 +0530

    Introduce AirportServiceIMPL

---
+++ src/main/java/lk/ijse/cmjd112/AirTicketPoint/service/impl/AirportServiceIMPL.java
@@ -0,0 +1,33 @@
+package lk.ijse.cmjd112.AirTicketPoint.service.impl;
+
+import lk.ijse.cmjd112.AirTicketPoint.dto.AirportDTO;
+import lk.ijse.cmjd112.AirTicketPoint.service.AirportService;
+
+import java.util.List;
+
+public class AirportServiceIMPL implements AirportService {
+    @Override
+    public AirportDTO saveAirport(AirportDTO airportDTO) {
+        return null;
+    }
+
+    @Override
+    public AirportDTO getSelectedAirport(String airportId) {
+        return null;
+    }
+
+    @Override
+    public List<AirportDTO> getAllAirports() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteAirport(String airportId) {
+
+    }
+
+    @Override
+    public void updateAirport(String airportId, AirportDTO airportDTO) {
+
+    }
+}
