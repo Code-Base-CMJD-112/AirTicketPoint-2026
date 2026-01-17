4999ebf92c0421ce2390fd9d1e79fb40149edf41
ThanuraSilva <thanurasilva1989@gmail.com>
2026-01-17 10:34:04 +0530

    Create AirPortController and initially test save an airport Crate util for unique reference generate

---
+++ src/main/java/lk/ijse/cmjd112/AirTicketPoint/util/IDGenerator.java
@@ -0,0 +1,12 @@
+package lk.ijse.cmjd112.AirTicketPoint.util;
+
+import java.util.UUID;
+
+public class IDGenerator {
+    //Airport
+    public static String airportIDGen(){
+        return "APT-"+ UUID.randomUUID();
+    }
+
+
+}
