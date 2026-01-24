e5d791ee8fba6d207ec4b515e32c9da3b8457929
ThanuraSilva <thanurasilva1989@gmail.com>
2026-01-24 11:58:03 +0530

    Create AirportEntity

--- src/main/java/lk/ijse/cmjd112/AirTicketPoint/entities/AirportEntity.java
+++ src/main/java/lk/ijse/cmjd112/AirTicketPoint/entities/AirportEntity.java
@@ -1,5 +1,8 @@
 package lk.ijse.cmjd112.AirTicketPoint.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "airport")
 public class AirportEntity {
+    @Id
     private String airportId;
     private String airportCode;
     private String airportName;
