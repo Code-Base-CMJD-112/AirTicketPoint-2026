a4999df2a83c004c1fe51eb6a41b6ef9e8d58580
ThanuraSilva <thanurasilva1989@gmail.com>
2026-02-28 13:10:44 +0530

    Ad CORS config

---
+++ src/main/java/lk/ijse/cmjd112/AirTicketPoint/config/CORSConfig.java
@@ -0,0 +1,29 @@
+package lk.ijse.cmjd112.AirTicketPoint.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+        var corsConfig= new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setExposedHeaders(List.of("Authorization", "Content-Type"));
+        var corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+
+        var corsFilterBean = new FilterRegistrationBean<>(new org.springframework.web.filter.CorsFilter(corsConfigurationSource));
+        corsFilterBean.setOrder(0);
+        return corsFilterBean;
+    }
+}
